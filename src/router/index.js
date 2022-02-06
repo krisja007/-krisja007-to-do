db663e2351a54c2dd1ebf3dc408a3c24b037a207
Siri364 <gubeerza09@gmail.com>
2022-02-06 18:38:06 +0700

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import todo from "../views/todo.vue";
+
 
 Vue.use(VueRouter);
 
@@ -9,6 +11,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/todo",
+    name: "todo",
+    component: todo,
   },
   {
     path: "/about",
